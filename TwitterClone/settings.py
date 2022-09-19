1191db74887e80ce8fa1686d6b2c48ec8c225d66
jashanmalhi <jmalhi01@gmail.com>
2022-09-19 00:08:13 -0700

    DEBUG

--- TwitterClone/settings.py
+++ TwitterClone/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e1$wu8ce(_f9m53#_tx+=0o$j+kf_w98t47d_-a@ugynnfbrbl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
